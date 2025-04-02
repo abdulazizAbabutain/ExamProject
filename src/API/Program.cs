3030b4d7d820ac40ec64fcb049fd0cb83087bfeb
Abdulaziz-Albabtain <abdulaziz.albabtain@hey.com>
2025-04-02 20:11:32 +0300

    add the UI

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -19,11 +19,11 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowElectron", policy =>
+    options.AddPolicy("AllowAngularDevClient", policy =>
     {
-        policy.AllowAnyOrigin()
-              .AllowAnyMethod()
-              .AllowAnyHeader();
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
     });
 });
 
@@ -36,9 +36,29 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
     app.MapScalarApiReference();
 }
-app.UseCors("AllowElectron");
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    // During development, proxy to Angular's development server
+    app.UseSpa(spa =>
+    {
+        spa.Options.SourcePath = "ClientApp";
+        //spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
+    });
+}
+else
+{
+    app.UseSpa(spa =>
+    {
+        spa.Options.SourcePath = "ClientApp/dist";
+        // Add additional configurations for production if needed
+    });
+}
+app.UseCors("AllowAngularDevClient");
 
 app.UseAuthorization();
 
